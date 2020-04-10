40db18b9450f49de95f42ac87a751dd84f255a2a
mwleungac <mwleungac@oocl.com>
2020-04-10 12:07:53 +0800

    commit return_car_when_ticket_fetched

--- src/main/java/com/oocl/Car.java
+++ src/main/java/com/oocl/Car.java
@@ -1,17 +1,4 @@
 package com.oocl;
 
 public class Car {
-
-    private  boolean isParked;
-
-    public Car() {
-    }
-
-    public boolean isParked() {
-        return isParked;
-    }
-
-    public void setParked(boolean parked) {
-        isParked = parked;
-    }
 }
--- src/main/java/com/oocl/Ticket.java
+++ src/main/java/com/oocl/Ticket.java
@@ -1,30 +1,4 @@
 package com.oocl;
 
 public class Ticket {
-
-    private boolean isUsed;
-    private boolean isProvided;
-    private int code;
-
-    public Ticket() {
-    }
-    public Ticket(int code) {
-        this.code = code;
-    }
-    public Ticket(boolean isUsed, boolean isProvided) {
-        this.isUsed = isUsed;
-        this.isProvided = isProvided;
-    }
-
-    public boolean isUsed() {
-        return isUsed;
-    }
-
-    public boolean isProvided() {
-        return isProvided;
-    }
-
-    public int getCode() {
-        return code;
-    }
 }
